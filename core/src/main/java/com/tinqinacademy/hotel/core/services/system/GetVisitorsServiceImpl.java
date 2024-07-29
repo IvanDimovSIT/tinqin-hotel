553f1e64cb3b0da0cbcbc7a6d514a38765998143
IvanDimovSIT <ivan390@abv.bg>
2024-07-29 19:19:03 +0300

    Changed GetVisitorsServiceImpl to use CriteriaBuilder

--- core/src/main/java/com/tinqinacademy/hotel/core/services/system/GetVisitorsServiceImpl.java
+++ core/src/main/java/com/tinqinacademy/hotel/core/services/system/GetVisitorsServiceImpl.java
@@ -6,7 +6,12 @@ import com.tinqinacademy.hotel.api.operations.system.getvisitors.GetVisitorsOutp
 import com.tinqinacademy.hotel.api.operations.system.getvisitors.GetVisitorsService;
 import com.tinqinacademy.hotel.persistence.model.Booking;
 import com.tinqinacademy.hotel.persistence.model.Guest;
+import com.tinqinacademy.hotel.persistence.model.Room;
 import com.tinqinacademy.hotel.persistence.repository.BookingRepository;
+import com.tinqinacademy.hotel.persistence.repository.GuestRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Tuple;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.ConversionService;
@@ -19,57 +24,77 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 public class GetVisitorsServiceImpl implements GetVisitorsService {
-    private final BookingRepository bookingRepository;
     private final ConversionService conversionService;
+    private final EntityManager entityManager;
 
     @Override
     public GetVisitorsOutput process(GetVisitorsInput input) {
         log.info("Start getVisitors input:{}", input);
 
-        List<Booking> bookings = bookingRepository.findBookingsByCriteria(
-                input.getStartDate(),
-                input.getEndDate(),
-                input.getFirstName(),
-                input.getLastName(),
-                input.getPhoneNumber(),
-                input.getIdCardNumber(),
-                input.getIdCardValidity(),
-                input.getIdCardIssueAuthority(),
-                input.getIdCardIssueDate(),
-                input.getRoomNumber());
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Tuple> query = criteriaBuilder.createTupleQuery();
+        Root<Booking> bookingRoot = query.from(Booking.class);
+        Join<Booking, Guest> guestJoin = bookingRoot.join("guests");
+        Join<Booking, Room> roomJoin = bookingRoot.join("room");
 
-        List<VisitorOutput> visitorOutputs = new ArrayList<>();
-        for (Booking booking : bookings) {
-            for (Guest guest : booking.getGuests()) {
-                if (input.getFirstName() != null && !input.getFirstName().equals(guest.getFirstName())) {
-                    continue;
-                }
-                if (input.getLastName() != null && !input.getLastName().equals(guest.getLastName())) {
-                    continue;
-                }
-                if (input.getIdCardNumber() != null && !input.getIdCardNumber().equals(guest.getIdCardNo())) {
-                    continue;
-                }
-                if (input.getIdCardValidity() != null && !input.getIdCardValidity().equals(guest.getIdCardValidity())) {
-                    continue;
-                }
-                if (input.getIdCardIssueAuthority() != null && !input.getIdCardIssueAuthority().equals(guest.getIdCardIssueAuthority())) {
-                    continue;
-                }
-                if (input.getIdCardIssueDate() != null && !input.getIdCardIssueDate().equals(guest.getIdCardIssueDate())) {
-                    continue;
-                }
-                if (input.getPhoneNumber() != null && !input.getPhoneNumber().equals(guest.getPhoneNumber())) {
-                    continue;
-                }
-                VisitorOutput visitorOutput = conversionService.convert(guest, VisitorOutput.class);
-                visitorOutput.setRoomId(booking.getRoom().getId().toString());
-                visitorOutput.setStartDate(booking.getStartDate());
-                visitorOutput.setEndDate(booking.getEndDate());
+        List<Predicate> predicates = new ArrayList<>();
 
-                visitorOutputs.add(visitorOutput);
-            }
+        Predicate startDatePredicate = criteriaBuilder.between(
+                bookingRoot.get("startDate"), input.getStartDate(), input.getEndDate());
+        Predicate endDatePredicate = criteriaBuilder.between(
+                bookingRoot.get("endDate"), input.getStartDate(), input.getEndDate());
+        Predicate overlapStartDatePredicate = criteriaBuilder.lessThanOrEqualTo(
+                bookingRoot.get("startDate"), input.getStartDate());
+        Predicate overlapEndDatePredicate = criteriaBuilder.greaterThanOrEqualTo(
+                bookingRoot.get("endDate"), input.getEndDate());
+
+        predicates.add(criteriaBuilder.or(startDatePredicate, endDatePredicate,
+                criteriaBuilder.and(overlapStartDatePredicate, overlapEndDatePredicate)));
+
+        if (input.getFirstName() != null) {
+            predicates.add(criteriaBuilder.like(guestJoin.get("firstName"), input.getFirstName()));
         }
+        if (input.getLastName() != null) {
+            predicates.add(criteriaBuilder.like(guestJoin.get("lastName"), input.getLastName()));
+        }
+        if (input.getPhoneNumber() != null) {
+            predicates.add(criteriaBuilder.equal(guestJoin.get("phoneNumber"), input.getPhoneNumber()));
+        }
+        if (input.getIdCardNumber() != null) {
+            predicates.add(criteriaBuilder.equal(guestJoin.get("idCardNo"), input.getIdCardNumber()));
+        }
+        if (input.getIdCardValidity() != null) {
+            predicates.add(criteriaBuilder.equal(guestJoin.get("idCardValidity"), input.getIdCardValidity()));
+        }
+        if (input.getIdCardIssueAuthority() != null) {
+            predicates.add(criteriaBuilder.like(guestJoin.get("idCardIssueAuthority"), input.getIdCardIssueAuthority()));
+        }
+        if (input.getIdCardIssueDate() != null) {
+            predicates.add(criteriaBuilder.equal(guestJoin.get("idCardIssueDate"), input.getIdCardIssueDate()));
+        }
+        if (input.getRoomNumber() != null) {
+            predicates.add(criteriaBuilder.like(roomJoin.get("roomNo"), input.getRoomNumber()));
+        }
+
+        query.where(predicates.toArray(new Predicate[0]));
+        query.multiselect(guestJoin, bookingRoot);
+
+        List<Tuple> results = entityManager.createQuery(query).getResultList();
+        List<VisitorOutput> visitorOutputs = results.stream()
+                .map(tuple -> {
+                    Guest guest = tuple.get(0, Guest.class);
+                    Booking booking = tuple.get(1, Booking.class);
+                    VisitorOutput visitorOutput = conversionService.convert(guest, VisitorOutput.class);
+                    if (visitorOutput != null) {
+                        visitorOutput.setRoomId(booking.getRoom().getId().toString());
+                        visitorOutput.setStartDate(booking.getStartDate());
+                        visitorOutput.setEndDate(booking.getEndDate());
+                    }
+
+                    return visitorOutput;
+                })
+                .toList();
+
 
         GetVisitorsOutput result = GetVisitorsOutput.builder()
                 .visitorOutputs(visitorOutputs)
