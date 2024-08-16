6cb49babb2b56fae58aa7717e8e0c4597c2b9b17
IvanDimovSIT <ivan390@abv.bg>
2024-08-16 12:19:38 +0300

    Added integration tests for HotelController and SystemController

---
+++ rest/src/test/java/com/tinqinacademy/hotel/rest/controllers/HotelControllerTests.java
@@ -0,0 +1,271 @@
+package com.tinqinacademy.hotel.rest.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tinqinacademy.hotel.api.RestApiRoutes;
+import com.tinqinacademy.hotel.api.operations.hotel.bookroom.BookRoomInput;
+import com.tinqinacademy.hotel.api.operations.hotel.unbookroom.UnbookRoomInput;
+import com.tinqinacademy.hotel.persistence.model.Bed;
+import com.tinqinacademy.hotel.persistence.model.Booking;
+import com.tinqinacademy.hotel.persistence.model.Guest;
+import com.tinqinacademy.hotel.persistence.model.Room;
+import com.tinqinacademy.hotel.persistence.model.enums.BathroomType;
+import com.tinqinacademy.hotel.persistence.model.enums.BedSize;
+import com.tinqinacademy.hotel.persistence.repository.BedRepository;
+import com.tinqinacademy.hotel.persistence.repository.BookingRepository;
+import com.tinqinacademy.hotel.persistence.repository.GuestRepository;
+import com.tinqinacademy.hotel.persistence.repository.RoomRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY, connection = EmbeddedDatabaseConnection.H2)
+@Transactional
+public class HotelControllerTests {
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private ObjectMapper mapper;
+    @Autowired
+    private RoomRepository roomRepository;
+    @Autowired
+    private BedRepository bedRepository;
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    private Room room;
+    private Booking booking;
+
+    @BeforeEach
+    public void setup() {
+
+        Bed bed = bedRepository.findByBedSize(BedSize.SINGLE).get();
+
+        room = Room.builder()
+                .roomNo("123")
+                .bathroomType(BathroomType.SHARED)
+                .beds(List.of(bed))
+                .price(BigDecimal.valueOf(678.32))
+                .floor(3)
+                .build();
+
+        room = roomRepository.save(room);
+
+        booking = Booking.builder()
+                .startDate(LocalDate.of(2029, 9, 27))
+                .endDate(LocalDate.of(2029, 9, 29))
+                .room(room)
+                .userId(UUID.randomUUID())
+                .totalPrice(BigDecimal.valueOf(1000))
+                .guests(new HashSet<>())
+                .build();
+
+        booking = bookingRepository.save(booking);
+
+    }
+
+    @Test
+    public void testCheckAvailableRoomsOk() throws Exception {
+        mvc.perform(get(RestApiRoutes.HOTEL_GET_AVAILABLE_ROOMS)
+                        .param("startDate", "2029-08-20")
+                        .param("endDate", "2029-08-25")
+                        .param("bedCount", "1")
+                        .param("bedSize", "single")
+                        .param("bathroomType", "shared")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testCheckAvailableRoomsBadRequest() throws Exception {
+        mvc.perform(get(RestApiRoutes.HOTEL_GET_AVAILABLE_ROOMS)
+                        .param("startDate", "2029-08-20")
+                        .param("endDate", "")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testGetRoomOk() throws Exception {
+        mvc.perform(get(RestApiRoutes.HOTEL_GET_ROOM, room.getId().toString())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testGetRoomNotFound() throws Exception {
+        mvc.perform(get(RestApiRoutes.HOTEL_GET_ROOM, UUID.randomUUID().toString())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testGetRoomBadRequest() throws Exception {
+        mvc.perform(get(RestApiRoutes.HOTEL_GET_ROOM, "123")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testBookRoomCreated() throws Exception {
+        BookRoomInput input = BookRoomInput.builder()
+                .userId(UUID.randomUUID().toString())
+                .startDate(LocalDate.of(2029, 8, 20))
+                .endDate(LocalDate.of(2029, 8, 25))
+                .build();
+
+        mvc.perform(post(RestApiRoutes.HOTEL_BOOK_ROOM, room.getId().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void testBookRoomBadRequestBookingInThePast() throws Exception {
+        BookRoomInput input = BookRoomInput.builder()
+                .userId(UUID.randomUUID().toString())
+                .startDate(LocalDate.of(2004, 8, 20))
+                .endDate(LocalDate.of(2004, 8, 25))
+                .build();
+
+        mvc.perform(post(RestApiRoutes.HOTEL_BOOK_ROOM, room.getId().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testBookRoomAlreadyBooked() throws Exception {
+        BookRoomInput input = BookRoomInput.builder()
+                .userId(UUID.randomUUID().toString())
+                .startDate(booking.getStartDate())
+                .endDate(booking.getEndDate())
+                .build();
+
+        mvc.perform(post(RestApiRoutes.HOTEL_BOOK_ROOM, booking.getRoom().getId().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testBookRoomNotFound() throws Exception {
+        BookRoomInput input = BookRoomInput.builder()
+                .userId(UUID.randomUUID().toString())
+                .startDate(LocalDate.of(2029, 8, 20))
+                .endDate(LocalDate.of(2029, 8, 25))
+                .build();
+
+        mvc.perform(post(RestApiRoutes.HOTEL_BOOK_ROOM, UUID.randomUUID().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testBookRoomBadRequestInvalidRoomId() throws Exception {
+        BookRoomInput input = BookRoomInput.builder()
+                .userId(UUID.randomUUID().toString())
+                .startDate(LocalDate.of(2029, 8, 20))
+                .endDate(LocalDate.of(2029, 8, 25))
+                .build();
+
+        mvc.perform(post(RestApiRoutes.HOTEL_BOOK_ROOM, "234")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testBookRoomBadRequestInvalidRequestBody() throws Exception {
+        BookRoomInput input = BookRoomInput.builder()
+                .userId(UUID.randomUUID().toString())
+                .startDate(LocalDate.of(2029, 8, 20))
+                .build();
+
+        mvc.perform(post(RestApiRoutes.HOTEL_BOOK_ROOM, room.getId().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testUnbookRoomOk() throws Exception {
+        UnbookRoomInput input = UnbookRoomInput.builder()
+                .userId(booking.getUserId().toString())
+                .build();
+
+        mvc.perform(delete(RestApiRoutes.HOTEL_UNBOOK_ROOM, booking.getId().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testUnbookRoomBadRequestUserDoesntOwnBooking() throws Exception {
+        UnbookRoomInput input = UnbookRoomInput.builder()
+                .userId(UUID.randomUUID().toString())
+                .build();
+
+        mvc.perform(delete(RestApiRoutes.HOTEL_UNBOOK_ROOM, booking.getId().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testUnbookRoomBadRequestInvalidBody() throws Exception {
+        UnbookRoomInput input = UnbookRoomInput.builder()
+                .build();
+
+        mvc.perform(delete(RestApiRoutes.HOTEL_UNBOOK_ROOM, booking.getId().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testUnbookRoomBadRequestInvalidRoomId() throws Exception {
+        UnbookRoomInput input = UnbookRoomInput.builder()
+                .userId(booking.getUserId().toString())
+                .build();
+
+        mvc.perform(delete(RestApiRoutes.HOTEL_UNBOOK_ROOM, "123")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testUnbookRoomNotFound() throws Exception {
+        UnbookRoomInput input = UnbookRoomInput.builder()
+                .userId(booking.getUserId().toString())
+                .build();
+
+        mvc.perform(delete(RestApiRoutes.HOTEL_UNBOOK_ROOM, UUID.randomUUID().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isNotFound());
+    }
+}
---
+++ rest/src/test/java/com/tinqinacademy/hotel/rest/controllers/SystemControllerTests.java
@@ -0,0 +1,358 @@
+package com.tinqinacademy.hotel.rest.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tinqinacademy.hotel.api.RestApiRoutes;
+import com.tinqinacademy.hotel.api.model.visitor.VisitorInput;
+import com.tinqinacademy.hotel.api.operations.system.addroom.AddRoomInput;
+import com.tinqinacademy.hotel.api.operations.system.partialupdateroom.PartialUpdateRoomInput;
+import com.tinqinacademy.hotel.api.operations.system.registervisitor.RegisterVisitorInput;
+import com.tinqinacademy.hotel.api.operations.system.updateroom.UpdateRoomInput;
+import com.tinqinacademy.hotel.persistence.model.Bed;
+import com.tinqinacademy.hotel.persistence.model.Booking;
+import com.tinqinacademy.hotel.persistence.model.Room;
+import com.tinqinacademy.hotel.persistence.model.enums.BathroomType;
+import com.tinqinacademy.hotel.persistence.model.enums.BedSize;
+import com.tinqinacademy.hotel.persistence.repository.BedRepository;
+import com.tinqinacademy.hotel.persistence.repository.BookingRepository;
+import com.tinqinacademy.hotel.persistence.repository.RoomRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY, connection = EmbeddedDatabaseConnection.H2)
+@Transactional
+public class SystemControllerTests {
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private ObjectMapper mapper;
+    @Autowired
+    private RoomRepository roomRepository;
+    @Autowired
+    private BedRepository bedRepository;
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    private Room room;
+    private Booking booking;
+
+    @BeforeEach
+    public void setup() {
+
+        Bed bed = bedRepository.findByBedSize(BedSize.SINGLE).get();
+
+        room = Room.builder()
+                .roomNo("123")
+                .bathroomType(BathroomType.SHARED)
+                .beds(List.of(bed))
+                .price(BigDecimal.valueOf(678.32))
+                .floor(3)
+                .build();
+
+        room = roomRepository.save(room);
+
+        booking = Booking.builder()
+                .startDate(LocalDate.of(2029, 9, 27))
+                .endDate(LocalDate.of(2029, 9, 29))
+                .room(room)
+                .userId(UUID.randomUUID())
+                .totalPrice(BigDecimal.valueOf(1000))
+                .guests(new HashSet<>())
+                .build();
+
+        booking = bookingRepository.save(booking);
+
+    }
+
+    @Test
+    public void testRegisterVisitorCreated() throws Exception {
+        VisitorInput visitorInput = VisitorInput.builder()
+                .firstName("Kolyo")
+                .lastName("Zafirov")
+                .idCardNumber("123354596")
+                .idCardIssueAuthority("ABCDEF")
+                .idCardValidity(LocalDate.now())
+                .idCardIssueDate(LocalDate.now())
+                .phoneNumber(null)
+                .dateOfBirth(LocalDate.of(2000, 10, 4))
+                .startDate(booking.getStartDate())
+                .endDate(booking.getEndDate())
+                .roomId(booking.getRoom().getId().toString())
+                .build();
+
+        RegisterVisitorInput input = RegisterVisitorInput.builder()
+                .visitorInputs(List.of(visitorInput))
+                .build();
+
+        mvc.perform(post(RestApiRoutes.SYSTEM_REGISTER_VISITOR)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testRegisterVisitorBadRequest() throws Exception {
+        VisitorInput visitorInput = VisitorInput.builder()
+                .firstName("Kolyo")
+                .lastName("Zafirov")
+                .idCardNumber("1233545")
+                .idCardIssueAuthority("ABCDEF")
+                .idCardValidity(LocalDate.now())
+                .idCardIssueDate(LocalDate.now())
+                .phoneNumber(null)
+                .dateOfBirth(LocalDate.of(2000, 10, 4))
+                .startDate(LocalDate.of(2030, 10, 12))
+                .endDate(LocalDate.of(2030, 10, 12))
+                .roomId(room.getId().toString())
+                .build();
+
+        RegisterVisitorInput input = RegisterVisitorInput.builder()
+                .visitorInputs(List.of(visitorInput))
+                .build();
+
+        mvc.perform(post(RestApiRoutes.SYSTEM_REGISTER_VISITOR)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testGetVisitorsOk() throws Exception {
+        mvc.perform(get(RestApiRoutes.SYSTEM_GET_VISITORS)
+                        .param("startDate", "2024-01-01")
+                        .param("endDate", "2024-12-31")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testGetVisitorsBadRequest() throws Exception {
+        mvc.perform(get(RestApiRoutes.SYSTEM_GET_VISITORS)
+                        .param("startDate", "abc")
+                        .param("endDate", "2024-12-31")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testAddRoomCreated() throws Exception {
+        AddRoomInput input = AddRoomInput.builder()
+                .bedCount(2)
+                .price(BigDecimal.valueOf(600))
+                .floor(5)
+                .bedSize(com.tinqinacademy.hotel.api.model.enums.BedSize.SINGLE)
+                .bathroomType(com.tinqinacademy.hotel.api.model.enums.BathroomType.PRIVATE)
+                .roomNumber("101")
+                .build();
+
+        mvc.perform(post(RestApiRoutes.SYSTEM_ADD_ROOM)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void testAddRoomBadRequest() throws Exception {
+        AddRoomInput input = AddRoomInput.builder()
+                .roomNumber("123")
+                .build();
+
+        mvc.perform(post(RestApiRoutes.SYSTEM_ADD_ROOM)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testUpdateRoomOk() throws Exception {
+        UpdateRoomInput input = UpdateRoomInput.builder()
+                .roomId(room.getId().toString())
+                .bedCount(2)
+                .price(BigDecimal.valueOf(600))
+                .floor(5)
+                .bedSize(com.tinqinacademy.hotel.api.model.enums.BedSize.SINGLE)
+                .bathroomType(com.tinqinacademy.hotel.api.model.enums.BathroomType.PRIVATE)
+                .roomNumber("101")
+                .build();
+
+        mvc.perform(put(RestApiRoutes.SYSTEM_UPDATE_ROOM, input.getRoomId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testUpdateRoomNotFound() throws Exception {
+        UpdateRoomInput input = UpdateRoomInput.builder()
+                .roomId(UUID.randomUUID().toString())
+                .bedCount(2)
+                .price(BigDecimal.valueOf(600))
+                .floor(5)
+                .bedSize(com.tinqinacademy.hotel.api.model.enums.BedSize.SINGLE)
+                .bathroomType(com.tinqinacademy.hotel.api.model.enums.BathroomType.PRIVATE)
+                .roomNumber("101")
+                .build();
+
+        mvc.perform(put(RestApiRoutes.SYSTEM_UPDATE_ROOM, UUID.randomUUID().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testUpdateRoomBadRequestInvalidRequestBody() throws Exception {
+        UpdateRoomInput input = UpdateRoomInput.builder()
+                .roomId(UUID.randomUUID().toString())
+                .bedCount(2)
+                .price(BigDecimal.valueOf(600))
+                .floor(-1329)
+                .bedSize(com.tinqinacademy.hotel.api.model.enums.BedSize.SINGLE)
+                .bathroomType(com.tinqinacademy.hotel.api.model.enums.BathroomType.PRIVATE)
+                .roomNumber("101")
+                .build();
+
+        mvc.perform(put(RestApiRoutes.SYSTEM_UPDATE_ROOM, room.getId().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isBadRequest());
+    }
+
+
+    @Test
+    public void testUpdateRoomBadRequestInvalidId() throws Exception {
+        UpdateRoomInput input = UpdateRoomInput.builder()
+                .roomId(UUID.randomUUID().toString())
+                .bedCount(2)
+                .price(BigDecimal.valueOf(600))
+                .floor(4)
+                .bedSize(com.tinqinacademy.hotel.api.model.enums.BedSize.SINGLE)
+                .bathroomType(com.tinqinacademy.hotel.api.model.enums.BathroomType.PRIVATE)
+                .roomNumber("101")
+                .build();
+
+        mvc.perform(put(RestApiRoutes.SYSTEM_UPDATE_ROOM,"1236")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testPartialUpdateRoomOk() throws Exception {
+        PartialUpdateRoomInput input = PartialUpdateRoomInput.builder()
+                .price(BigDecimal.valueOf(1235))
+                .build();
+
+        Room roomToUpdate = Room.builder()
+                .roomNo("344")
+                .bathroomType(BathroomType.SHARED)
+                .beds(new ArrayList<>(List.of(bedRepository.findByBedSize(BedSize.SINGLE).get())))
+                .price(BigDecimal.valueOf(100.77))
+                .floor(3)
+                .build();
+
+        roomToUpdate = roomRepository.save(roomToUpdate);
+
+        mvc.perform(patch(RestApiRoutes.SYSTEM_PARTIAL_UPDATE_ROOM, roomToUpdate.getId().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testPartialUpdateRoomBadRequest() throws Exception {
+        PartialUpdateRoomInput input = PartialUpdateRoomInput.builder()
+                .price(BigDecimal.valueOf(1235))
+                .build();
+
+        Room roomToUpdate = Room.builder()
+                .roomNo("344")
+                .bathroomType(BathroomType.SHARED)
+                .beds(new ArrayList<>(List.of(bedRepository.findByBedSize(BedSize.SINGLE).get())))
+                .price(BigDecimal.valueOf(100.77))
+                .floor(3)
+                .build();
+
+        roomToUpdate = roomRepository.save(roomToUpdate);
+
+        mvc.perform(patch(RestApiRoutes.SYSTEM_PARTIAL_UPDATE_ROOM, "123423")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testPartialUpdateRoomNotFound() throws Exception {
+        PartialUpdateRoomInput input = PartialUpdateRoomInput.builder()
+                .roomId(UUID.randomUUID().toString())
+                .floor(3)
+                .build();
+
+        mvc.perform(patch(RestApiRoutes.SYSTEM_PARTIAL_UPDATE_ROOM, UUID.randomUUID().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testDeleteRoomOk() throws Exception {
+        Room roomToDelete = Room.builder()
+                .roomNo("456")
+                .bathroomType(BathroomType.SHARED)
+                .beds(List.of(bedRepository.findByBedSize(BedSize.SINGLE).get()))
+                .price(BigDecimal.valueOf(678.32))
+                .floor(3)
+                .build();
+
+        roomToDelete = roomRepository.save(roomToDelete);
+
+        mvc.perform(delete(RestApiRoutes.SYSTEM_DELETE_ROOM, roomToDelete.getId().toString())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testDeleteRoomBadRequest() throws Exception {
+        Room roomToDelete = Room.builder()
+                .roomNo("456")
+                .bathroomType(BathroomType.SHARED)
+                .beds(List.of(bedRepository.findByBedSize(BedSize.SINGLE).get()))
+                .price(BigDecimal.valueOf(678.32))
+                .floor(3)
+                .build();
+
+        roomToDelete = roomRepository.save(roomToDelete);
+
+        mvc.perform(delete(RestApiRoutes.SYSTEM_DELETE_ROOM, "123")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testDeleteRoomNotFound() throws Exception {
+        mvc.perform(delete(RestApiRoutes.SYSTEM_DELETE_ROOM, UUID.randomUUID().toString())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+}
