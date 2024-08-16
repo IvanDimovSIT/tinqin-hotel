9723800af49ba7c46b3098099959fc208c3684ee
Ivan Dimov <100278096+IvanDimovSIT@users.noreply.github.com>
2024-08-16 12:20:38 +0300

    [T33] Added integration tests for HotelController and SystemController (#72)
    Added integration tests for HotelController and SystemController using H2 database

---
+++ rest/src/test/java/com/tinqinacademy/hotel/rest/HotelApplicationTests.java
@@ -0,0 +1,11 @@
+package com.tinqinacademy.hotel.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HotelApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
